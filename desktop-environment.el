284c38fcb5b10746255ee4123eaa549e028df0c8
Damien Cassou <damien@cassou.me>
2019-05-26 10:28:23 +0200

    Release 0.2.2

--- desktop-environment.el
+++ desktop-environment.el
@@ -5,7 +5,7 @@
 ;; Author: Damien Cassou <damien@cassou.me>, Nicolas Petton <nicolas@petton.fr>
 ;; Url: https://gitlab.petton.fr/DamienCassou/desktop-environment
 ;; Package-requires: ((emacs "25.1"))
-;; Version: 0.2.1
+;; Version: 0.2.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
